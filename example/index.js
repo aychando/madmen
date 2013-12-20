5a4db1a022656a0cac2d8bd0aaaf13668e9b83e4
Andrew <andrew.yu@cirruscpq.com>
2013-12-19 18:25:55 -0800

    front end changes to madmen made

--- example/index.js
+++ example/index.js
@@ -2,12 +2,15 @@ var express   = require('express'),
     http      = require('http'),
     madmen    = require('../lib'),
     mongoose  = require('mongoose'),
+    // setting  'app' to the return of express() function, which is an application with only defaults
+    // which we modify using the express functions (e.g. app.use(), app.get(), app.configure().. )
     app       = express();
 
 // defining schema of mongoDB
 var kittySchema = mongoose.Schema({
     name: String,
     age: String,
+    food: String
 });
 // compiling schema to a model
 var Kitten = mongoose.model('Kitten', kittySchema);
@@ -21,12 +24,16 @@ app.configure(function(){
 // dev defines the root 'route', specified as '/kittens', the base url we define everything at
 // calling the 'madmen' function and passing in the object as a param
 // mounting madmen subapp @ '/admin'
-app.use('/admin', madmen( {
+
+var adminSubapp = madmen({
   routes : { 
     '/kittens' : 'Kitten'
   },
   basePath : '/admin'
-}));
+})
+
+// adminSubapp is the return of madmen() as an object (the express app!)
+app.use('/admin', adminSubapp);
 
 var server = http.createServer(app);
 server.listen(3001, function(){
@@ -34,11 +41,5 @@ server.listen(3001, function(){
 });
 
 
-app.use(madmen({
-  routes : {
-    route :
-  }
-  basePath : 
-})
+(function a(){console.log('Hi')})();
 
-  )
